543741d37f700be5efc7a1d5aceb115b63ab24d5
Saurav <sauravmohanta.dev@gmail.com>
2025-02-09 02:40:24 +0530

    complited the app

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,17 @@
+import { WebSocketServer } from "ws";
+import { client } from "@repo/db/client";
+
+const server = new WebSocketServer({
+  port: 8080,
+});
+
+server.on("connection", async (socket) => {
+  const user = await client.user.create({
+    data: {
+      username: Math.random().toString(),
+      password: Math.random().toString(),
+    },
+  });
+
+  socket.send("HI there you are connected to the server");
+});
